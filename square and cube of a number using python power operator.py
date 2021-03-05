21905c0672dce356ae2b0fa36819e78378a1cabf
S-SANTHI25 <52491210+S-SANTHI25@users.noreply.github.com>
2021-03-05 23:10:44 +0530

    Create square and cube of a number using python power operator.py

---
+++ square and cube of a number using python power operator.py
@@ -0,0 +1,11 @@
+#SQUARE AND CUBE OF A NUMBER USING PYTHON POWER OPERATOR
+
+number = int(input("Enter a number:"))
+
+#power operator : **
+#number**2 returns square, number**3 returns cube 
+
+print("Square value is:",number**2)
+print("Cube value is:",number**3)
+
+#Square and Cube value found
